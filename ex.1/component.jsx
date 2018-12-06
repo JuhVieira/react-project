70ea119810f942a2239c780c0916776b00410d5b
JuhVieira <julidev.28@gmail.com>
2018-12-05 21:03:55 -0300

    ultimocommit

---
+++ ex.1/component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Primeiro = props => (
+    <h1>Primeiro Component</h1>
+)
+
+const Segundo = props => (
+    <h1>Segundo Component</h1>
+)
+
+
+export {Primeiro, Segundo}
